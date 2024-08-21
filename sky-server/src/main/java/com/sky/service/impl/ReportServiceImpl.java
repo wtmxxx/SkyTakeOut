c8418f7359819d7119d635044ac3cf163322c17b
Wotemo <717542214@qq.com>
2024-08-21 22:21:52 +0800

    用户统计业务开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,9 +1,13 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sky.entity.Orders;
+import com.sky.entity.User;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,9 +23,12 @@ import java.util.Map;
 @Service
 public class ReportServiceImpl implements ReportService {
     private final OrderMapper orderMapper;
+    private final UserMapper userMapper;
+
     @Autowired
-    public ReportServiceImpl(OrderMapper orderMapper) {
+    public ReportServiceImpl(OrderMapper orderMapper, UserMapper userMapper) {
         this.orderMapper = orderMapper;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -55,6 +62,38 @@ public class ReportServiceImpl implements ReportService {
                 .builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        // 每天新增用户数量
+        List<Long> newUserList = new ArrayList<>();
+        // 每天总用户数量
+        List<Long> totalUserList = new ArrayList<>();
+        for (LocalDate localDate : dateList) {
+            LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper
+                    .ge(User::getCreateTime, LocalDateTime.of(localDate, LocalTime.MIN))
+                    .le(User::getCreateTime, LocalDateTime.of(localDate, LocalTime.MAX));
+            newUserList.add(userMapper.selectCount(lambdaQueryWrapper));
+
+            lambdaQueryWrapper.clear();
+            lambdaQueryWrapper.le(User::getCreateTime, LocalDateTime.of(localDate, LocalTime.MAX));
+            totalUserList.add(userMapper.selectCount(lambdaQueryWrapper));
+        }
+
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
                 .build();
     }
 }
